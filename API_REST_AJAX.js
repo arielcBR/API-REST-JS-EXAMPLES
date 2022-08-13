f6e85c2fdf274db64aa71f045436e2188c7a701a
Ariel <ariel.campos94@yahoo.com>
2022-08-12 23:16:35 -0300

    finalizado CRUD com AJAX

--- API_REST_AJAX.js
+++ API_REST_AJAX.js
@@ -1,46 +1,114 @@
-const d = document;
-const $table = d.querySelector("crud-table");
-const $form = d.querySelector("crud-form");
-const $title = d.querySelector("crud-title");
-const $template = d.getElementById("crud-template").content;
-const $fragment = d.createDocumentFragment();
+
+/* **********     Curso JavaScript: 115. APIs REST: CRUD con AJAX (1/2) - #jonmircha     ********** */
+/* **********     Curso JavaScript: 116. APIs REST: CRUD con AJAX (2/2) - #jonmircha     ********** */
+const d = document,
+  $table = d.querySelector(".crud-table"),
+  $form = d.querySelector(".crud-form"),
+  $title = d.querySelector(".crud-title"),
+  $template = d.getElementById("crud-template").content,
+  $fragment = d.createDocumentFragment();
 
 const ajax = (options) => {
-    let { url, method, success, error, data } = options;
-    const xhr = new XMLHttpRequest();
+  let { url, method, success, error, data } = options;
+  const xhr = new XMLHttpRequest();
 
-    xhr.addEventListener("readystatechange", e => {
-        if (xhr.readyState !== 4) return;
+  xhr.addEventListener("readystatechange", e => {
+    if (xhr.readyState !== 4) return;
 
-        if (xhr.status >= 200 && xhr.status < 300) {
-            // Converte a resposta da solicitação para texto
-            let json = JSON.parse(xhr.responseText);
-            // Success responsável por chamar o DOM e exibir no HTML a resposta
-            success(json);
-        }
-        else {
-            let message = xhr.statusText || "Ocurrió un error";
-            // Error responsável por chamar o DOM e exibir no HTML o erro
-            error(`Error ${xhr.status} ${message}`);
-        }
-    })
+    if (xhr.status >= 200 && xhr.status < 300) {
+      let json = JSON.parse(xhr.responseText);
+      success(json);
+    } else {
+      let message = xhr.statusText || "Ocurrió un error";
+      error(`Error ${xhr.status}: ${message}`);
+    }
+  });
 
-    // Especifica o Método e o endereço de onde buscar
-    xhr.open(method || "GET", url);                             
-        
-    // Especifica como o conteúdo será solicitado
-    xhr.setRequestHeader("Content-type", "application/json; charset=utf8")         
-    
-    // Conversão de JSON para string
-    xhr.send(JSON.stringify(data));
+  xhr.open(method || "GET", url);
+  xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+  xhr.send(JSON.stringify(data));
 }
 
 const getAll = () => {
-    ajax({
-        url: "http://localhost:3000/santos",
-        success: (res) => {console.log(res)},
-        error: (err) => {console.error(err)}
-    })
+  ajax({
+    url: "http://localhost:3000/santos",
+    success: (res) => {
+      console.log(res);
+
+      res.forEach(el => {
+        $template.querySelector(".name").textContent = el.nombre;
+        $template.querySelector(".constellation").textContent = el.constelacion;
+        $template.querySelector(".edit").dataset.id = el.id;
+        $template.querySelector(".edit").dataset.name = el.nombre;
+        $template.querySelector(".edit").dataset.constellation = el.constelacion;
+        $template.querySelector(".delete").dataset.id = el.id;
+
+        let $clone = d.importNode($template, true);
+        $fragment.appendChild($clone);
+      });
+
+      $table.querySelector("tbody").appendChild($fragment);
+    },
+    error: (err) => {
+      console.log(err);
+      $table.insertAdjacentHTML("afterend", `<p><b>${err}</b></p>`);
+    }
+  })
 }
 
-d.addEventListener("DOMContentLoaded", getAll)
+d.addEventListener("DOMContentLoaded", getAll);
+
+d.addEventListener("submit", e => {
+  if (e.target === $form) {
+    e.preventDefault();
+
+    if (!e.target.id.value) {
+      //Create - POST
+      ajax({
+        url: "http://localhost:3000/santos",
+        method: "POST",
+        success: (res) => location.reload(),
+        error: (err) => $form.insertAdjacentHTML("afterend", `<p><b>${err}</b></p>`),
+        data: {
+          nombre: e.target.nombre.value,
+          constelacion: e.target.constelacion.value
+        }
+      });
+    } else {
+      //Update - PUT
+      ajax({
+        url: `http://localhost:3000/santos/${e.target.id.value}`,
+        method: "PUT",
+        success: (res) => location.reload(),
+        error: (err) => $form.insertAdjacentHTML("afterend", `<p><b>${err}</b></p>`),
+        data: {
+          nombre: e.target.nombre.value,
+          constelacion: e.target.constelacion.value
+        }
+      });
+    }
+  }
+});
+
+d.addEventListener("click", e => {
+  if (e.target.matches(".edit")) {
+    $title.textContent = "Editar Santo";
+    $form.nombre.value = e.target.dataset.name;
+    $form.constelacion.value = e.target.dataset.constellation;
+    $form.id.value = e.target.dataset.id;
+  }
+
+  if (e.target.matches(".delete")) {
+    let isDelete = confirm(`¿Estás seguro de eliminar el id ${e.target.dataset.id}?`);
+
+    if (isDelete) {
+      //Delete - DELETE
+      ajax({
+        url: `http://localhost:3000/santos/${e.target.dataset.id}`,
+        method: "DELETE",
+        success: (res) => location.reload(),
+        error: (err) => alert(err)
+      });
+    }
+  }
+})
